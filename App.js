85edfb5391cdf4941540c5df9c3688197e4a5135
= <=>
2022-12-25 14:23:25 +0530

    Create Element using React APIs

---
+++ App.js
@@ -0,0 +1,27 @@
+const headingOne = React.createElement(
+  "h1",
+  {
+    id: "titleOne",
+  },
+  "Heading One"
+);
+
+const headingTwo = React.createElement(
+  "h2",
+  {
+    id: "titleTwo",
+  },
+  "Heading Two"
+);
+
+const Container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [headingOne, headingTwo]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(Container);
